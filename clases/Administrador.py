7ba327d2bdca334c77760608783f95a5a9e7bcfc
TioMicho <megaliostiaganzosorrex@gmail.com>
2022-12-19 16:20:33 -0300

    prototipo de metodos administrador

--- clases/Administrador.py
+++ clases/Administrador.py
@@ -1,3 +1,5 @@
+from clases import Salas
+from clases import Descuentos
 class Administrado():
 	def __init__(self,contraseña = None):
 		self.__usuario_id = None
@@ -17,20 +19,26 @@ class Administrado():
 	def historial_de_salas():
 		pass
 
-	def crearSala():
-		pass
+	def crearSala(self):
+		lS=Salas.Salas()
+		lS.agregarSala()
 	
-	def modificaSala():
-		pass
+	def modificaSala(self):
+		lS=Salas.Salas()
+		print(lS)
+		op=input("ingrese id de la sala a modificar")
+		lS.modificar(op)
 
-	def verSalas():
-		
-		pass
+	def verSalas(sef):
+		lS=Salas.Salas()
+		print(lS)
 
-	def modificarDescuentos():
-		pass
+	def modificarDescuentos(self):
+		desc=Descuentos.Descuentos()
+		desc.modificarDescuento()
 
-	def ver_la_disponibilidad_de_butacas():
+	def ver_la_disponibilidad_de_butacas(self,idSala):
+
 		pass
 
 	def gestion_de_venta_y_descuentos():
